c1087908a1c210b5f4c2d4a783a6b897f43f1191
Sebastián Gómez Morales <sebastian61910.sg@gmail.com>
2021-02-17 10:26:12 -0700

    Se agrega el programa del InverseHelloWorld

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P2_InverseHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un arreglo con los caracteres que conforman la frase "Hello
+            //World!". Invertir el orden de los caracteres, e imprimir el arreglo
+            //resultante.
+            //Se debe imprimir lo siguiente:
+            //!dlroW olleH
+
+            string[] caracteres = new string[12] {"H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", "!"};
+
+            for(int i = 11; i >= 0; i--)
+            {
+                Console.Write(caracteres[i]);
+            }
+
+        }
+    }
+}
